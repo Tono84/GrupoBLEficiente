934dc76081a890980ee75bff52ced3e5f8a06add
José Antonio Solórzano Flores <98479861+Tono84@users.noreply.github.com>
2024-02-07 19:51:09 -0600

    diferentes vistas

--- GrupoBLEficiente/GrupoBLEficiente/Models/TaxDeduction.cs
+++ GrupoBLEficiente/GrupoBLEficiente/Models/TaxDeduction.cs
@@ -1,13 +1,21 @@
-﻿namespace GrupoBLEficiente.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrupoBLEficiente.Models
 {
     public class TaxDeduction
     {
+        [Key]
         public int IdTaxDeduction { get; set; }
 
+        [Required(ErrorMessage = "El Nombre es requerido")]
+        [Display(Name = "Nombre")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "El Porcentage de la Deducción es requerido")]
+        [Display(Name = "Porcentaje de Deducción")]
         public Double Percentage { get; set; }
 
+        [Display(Name = "Descripción")]
         public string Description { get; set; }
 
         public List<Paysheet>? Paysheets { get; set; }
